42cc667c34dd34713c9616a2e555fbadf377cfff
egwilborn <egwilborn@gmail.com>
2023-10-13 17:15:24 -0500

    adds find_highest_price function

--- python-daily-coding.py
+++ python-daily-coding.py
@@ -352,3 +352,54 @@ def merge_objects(*args):
 # print(merge_objects({"a": 1, "b": 2, "c": 3}, {"d": 4}))
 # # //=> {a: 1, b: 22, c: 3, d: 44}
 # print(merge_objects({"a": 1, "b": 2, "c": 3}, {"d": 4}, {"b": 22, "d": 44}))
+
+# /*-----------------------------------------------------------------
+# Challenge: 16-findHighestPriced
+# Difficulty:  Intermediate
+# Prompt:
+# - Write a function named findHighestPriced that accepts a single array of objects.
+# - The objects contained in the array are guaranteed to have a price property holding a numeric value.
+# - The function should return the object in the array that has the largest value held in the price property.
+# - If there's a tie between two or more objects, return the first of those objects in the array.
+# - Return the original object, not a copy.
+# - DO NOT mutate the array, i.e., do not sort it
+# Examples:
+# findHighestPriced([
+#   { sku: 'a1', price: 25 },
+#   { sku: 'b2', price: 5 },
+#   { sku: 'c3', price: 50 },
+#   { sku: 'd4', price: 10 }
+# ]);
+# //=> { sku: 'c3', price: 50 }
+# findHighestPriced([
+#   { sku: 'a1', price: 25 },
+#   { sku: 'b2', price: 50 },
+#   { sku: 'c3', price: 50 },
+#   { sku: 'd4', price: 10 }
+# ]);
+# //=> { sku: 'b2', price: 50 }
+# -----------------------------------------------------------------*/
+def find_highest_priced(object_arr):
+    max_price = 0
+    max_price_idx = 0
+    for idx, object in enumerate(object_arr):
+        if (object["price"] > max_price):
+            max_price = object["price"]
+            max_price_idx = idx
+    return object_arr[max_price_idx]
+
+
+# print(find_highest_priced([
+#     {"sku": 'a1', "price": 25},
+#     {"sku": 'b2', "price": 5},
+#     {"sku": 'c3', "price": 50},
+#     {"sku": 'd4', "price": 10}
+# ]))
+# # //=> { sku: 'c3', "price": 50 }
+# print(find_highest_priced([
+#     {"sku": 'a1', "price": 25},
+#     {"sku": 'b2', "price": 50},
+#     {"sku": 'c3', "price": 50},
+#     {"sku": 'd4', "price": 10}
+# ]))
+# //=> { sku: 'b2', price: 50 }
