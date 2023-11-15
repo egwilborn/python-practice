99ca72c641a68ba61d38c3cc79b11663d1bbc3ec
egwilborn <egwilborn@gmail.com>
2023-11-15 10:56:04 -0600

    adds fizzbuzz

--- python-daily-coding.py
+++ python-daily-coding.py
@@ -595,8 +595,55 @@ def solve(x):
 
     def sliding_window(arr, num):
         for i in range(sub_length, len(x)):
-            sub_list = x[(sub_length-i):i]
+            sub_list = x[(sub_length-i):i-1]
             print(sub_list)
+    sliding_window(x, sub_length)
 
 
-solve("1341")  # = 7.
+# solve("1341")  # = 7.
+
+
+# DESCRIPTION:
+# Traditionally in FizzBuzz, multiples of 3 are replaced by "Fizz" and multiples of 5 are replaced by "Buzz". But we could also play FizzBuzz with any other integer pair [n, m] whose multiples are replaced with Fizz and Buzz.
+
+# For a sequence of numbers, Fizzes, Buzzes and FizzBuzzes, find the numbers whose multiples are being replaced by Fizz and Buzz. Return them as an array [n, m]
+
+# The Fizz and Buzz numbers will always be integers between 1 and 50, and the sequence will have a maximum length of 100. The Fizz and Buzz numbers might be equal, and might be equal to 1.
+
+# Examples
+# Classic FizzBuzz; multiples of 3 are replaced by Fizz, multiples of 5 are replaced by Buzz:
+# [1, 2, "Fizz", 4, "Buzz", 6]  ==>  [3, 5]
+# Multiples of 2 are replaced by Fizz, multiples of 3 are replaced by Buzz:
+# [1, "Fizz", "Buzz", "Fizz", 5, "FizzBuzz"]  ==>  [2, 3]
+# Multiples of 2 are replaced by Fizz and Buzz:
+# [1, "FizzBuzz", 3, "FizzBuzz", 5, "FizzBuzz"]  ==>  [2, 2]
+# Fizz = 1, Buzz = 6:
+# ["Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "FizzBuzz"]  ==>  [1, 6]
+
+def reverse_fizz_buzz(array):
+    fizz_positions = []
+    buzz_positions = []
+    fizz_buzz_positions = []
+    for idx, item in enumerate(array):
+        if (item == "Fizz"):
+            fizz_positions.append(idx+1)
+        elif (item == "Buzz"):
+            buzz_positions.append(idx+1)
+        elif (item == "FizzBuzz"):
+            fizz_buzz_positions.append(idx+1)
+    if (len(fizz_positions) == 0 and len(buzz_positions) == 0):
+        return [fizz_buzz_positions[0], fizz_buzz_positions[0]]
+    elif (len(fizz_positions) == 0 and len(buzz_positions) > 0):
+        return [fizz_buzz_positions[0], buzz_positions[0]]
+    elif (len(fizz_positions) > 0 and len(buzz_positions) == 0):
+        return [fizz_positions[0], fizz_buzz_positions[0]]
+    else:
+        return [fizz_positions[0], buzz_positions[0]]
+
+
+print(reverse_fizz_buzz([1, 2, "Fizz", 4, "Buzz", 6]))  # [3, 5]
+print(reverse_fizz_buzz([1, "Fizz", "Buzz", "Fizz", 5, "FizzBuzz"]))  # [2,3]
+print(reverse_fizz_buzz(
+    [1, "FizzBuzz", 3, "FizzBuzz", 5, "FizzBuzz"]))  # [2,2]
+print(reverse_fizz_buzz(["Fizz", "Fizz", "Fizz", "Fizz",
+                         "Fizz", "FizzBuzz"]))  # [1,6]
