c1a82e90fc97e8081e60b00bf7d868745ab1fca6
egwilborn <egwilborn@gmail.com>
2023-11-28 20:25:43 -0600

    adds spoonerize function

--- python-daily-coding.py
+++ python-daily-coding.py
@@ -769,3 +769,35 @@ def is_leap_year(year):
         return True
     else:
         return False
+
+
+# A spoonerism is a spoken phrase in which the first letters of two of the words are swapped around, often with amusing results.
+
+# In its most basic form a spoonerism is a two word phrase in which only the first letters of each word are swapped:
+
+# "not picking" --> "pot nicking"
+
+# Your task is to create a function that takes a string of two words, separated by a space: words and returns a spoonerism of those words in a string, as in the above example. A "word" in the context of this kata can contain any of the letters A through Z in upper or lower case, and the numbers 0 to 9. Though spoonerisms are about letters in words in the domain of written and spoken language, numbers are included in the inputs for the random test cases and they require no special treatment.
+
+# NOTE: All input strings will contain only two words. Spoonerisms can be more complex. For example, three-word phrases in which the first letters of the first and last words are swapped: "pack of lies" --> "lack of pies" or more than one letter from a word is swapped: "flat battery --> "bat flattery" You are NOT expected to account for these, or any other nuances involved in spoonerisms.
+
+def spoonerize(words):
+    # split the two words into a list
+    words_list = words.split(" ")
+    word_one_list = list(words_list[0])
+    # for char in words_list[0]:
+    #     word_one_list.append(char)
+
+    word_two_list = list(words_list[1])
+    # for char in words_list[1]:
+    #     word_two_list.append(char)
+    # store the first letters in two variables
+    # edit the two words deleting the first two letters and store them into a new array
+    word_one_letter = word_one_list[0]
+    word_one_list[0] = word_two_list[0]
+    word_two_list[0] = word_one_letter
+    new_words_list = ["".join(word_one_list), "".join(word_two_list)]
+    return " ".join(new_words_list)
+
+
+print(spoonerize("not picking"))
